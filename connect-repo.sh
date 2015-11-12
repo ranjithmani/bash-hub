df4a9d37dacb2a22217336602316d412014a2488
Ranjith Mani <ranjithmani@outlook.com>
2015-11-12 07:46:14 +0530

    changed some version issues with redhat and oracle

--- connect-repo.sh
+++ connect-repo.sh
@@ -4,19 +4,21 @@
 #Pre-requisite : lsb_release binary
 #Created on : 11-Nov-2015
 #Created By : Ranjith Mani
-RELEASE=`lsb_release -d | awk '{print $4}'|cut -f1 -d .`
-if [ $RELEASE == 6 ]
-then
-OS=`lsb_release -d | awk '{print $2}'`
+INI=`lsb_release -d | awk '{print $2}'`
+if [ $INI = "Red" ]
+then 
+OS=`lsb_release -d | awk '{print $2$3}'`
+RELEASE=`lsb_release -d | awk '{print $8}'|cut -f1 -d .`
 else
-echo "Repo is not available for `lsb_release -d | awk '{print $2"-"$4}'` version"
+RELEASE=`lsb_release -d | awk '{print $4}'|cut -f1 -d .`
+OS=`lsb_release -d | awk '{print $2}'`
 fi
 
 
 oracle ()
 {
 echo "connecting to $OS repo"
-slee 2
+sleep 2
 cd /etc/yum.repos.d/
 test -d repobkp || mkdir repobkp
 test -f oracle6.repo
@@ -26,7 +28,7 @@ echo "$OS repo is already available"
 exit 2
 else
 echo "backing up all repos"
-mv *.repo repobkp/
+mv *.repo repobkp/ > /dev/null
 cat > oracle6.repo << EOF
 [oracle6-repo]
 name=Oracle Enterprise Linux 6.7
@@ -34,12 +36,13 @@ enabled=1
 baseurl=http://172.23.50.113/oracle6
 gpgcheck=0
 EOF
+echo "Repo has been set for $OS Enterprise Linux 6"
 fi
 }
 redhat ()
 {
 echo "connecting to $OS repo"
-slee 2
+sleep 2
 cd /etc/yum.repos.d/
 test -d repobkp || mkdir repobkp
 test -f rhel6.repo
@@ -48,14 +51,15 @@ then
 echo "$OS repo is already available"
 else
 echo "backing up all repos"
-mv *.repo repobkp/
-cat > rhel6.repo < EOF
+mv *.repo repobkp/ > /dev/null
+cat > rhel6.repo << EOF
 [Redhat-repo]
 name=Redhat Enterprise Linux 6.7
 enabled=1
 baseurl=http://172.23.50.112/rhel6
 gpgcheck=0
 EOF
+echo "Repo has been set for $OS Enterprise Linux 6"
 fi
 }
 # see how the functions are called.
@@ -63,9 +67,9 @@ case $OS in
         Oracle)
         oracle
         ;;
-        Redhat)
+        RedHat)
         redhat
         ;;
         *)
-        echo "Repo not available for `lsb_release -d | awk '{print $2"-"$4}'` "
+        echo "Repo not available for `lsb_release -d | awk -F ":" '{print $2}'` "
         esac
