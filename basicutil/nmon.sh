0443e95322ef9fb996d71b92573a7217b0c8fad1
Ranjith Mani <ranjithmani@outlook.com>
2016-02-09 08:21:02 +0530

    Create nmon.sh

---
+++ basicutil/nmon.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# USAGE: To rotate and start nmon capture 
+# script created : Shamelessly copied from sandeep script.
+RDAYS=30
+
+function rotate()
+{
+
+        cd /var/log/nmon_reports/
+
+        ls > rotate.txt
+
+        while read FILE
+        do
+                TDATE=`date +%s`
+                CDATE=`stat -c %Y $FILE`
+                DIFF=$(( 86400 * ${RDAYS} ))
+
+                ISDEL=$(( TDATE - CDATE ))
+
+                if [ $DIFF -le $ISDEL ]
+                then
+                        rm -rvf $FILE > /dev/null
+                fi
+
+        done < rotate.txt
+
+rm -f rotate.txt > /dev/null
+
+}
+
+cd /var/log/nmon_reports/
+/usr/bin/nmon -s 600 -c 144 -f -t -m /var/log/nmon_reports/
+chmod o+r /var/log/nmon_reports/*.nmon
+rotate
