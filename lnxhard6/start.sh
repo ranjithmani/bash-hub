fe70038552b2be99559d167fb913637706191e26
Ranjith Mani <ranjithmani@outlook.com>
2016-09-02 10:22:05 +0530

    Added kenel enable module

--- lnxhard6/start.sh
+++ lnxhard6/start.sh
@@ -3,4 +3,7 @@
 LOG=~/lnxhard.log
 #1.Disable unwanted services
 echo "`date +%T"-"%D`:Disabling unwanted services." >>$LOG
-./disb_serv 
+./disb_serv
+#2.Enable Kernel security
+echo "`date +%T"-"%D`:Enabling kernel security." >>$LOG 
+./enb_kern
