52e95c1063a4ce934a3641d7f908eab30800aa78
Ranjith Mani <ranjithmani@outlook.com>
2016-09-08 15:32:50 +0530

    Update start.sh

--- lnxhard6/start.sh
+++ lnxhard6/start.sh
@@ -2,8 +2,29 @@
 #
 LOG=~/lnxhard.log
 #1.Disable unwanted services
-echo "`date +%T"-"%D`:Disabling unwanted services." >>$LOG
+echo "`date +%D"-"%T`:Disabling unwanted services." >>$LOG
 ./disb_serv
 #2.Enable Kernel security
-echo "`date +%T"-"%D`:Enabling kernel security." >>$LOG 
+echo "`date +%D"-"%T`:Enabling kernel security." >>$LOG
 ./enb_kern
+#3.Set Password policy
+echo "`date +%D"-"%T`:Setting password policy." >>$LOG
+./enb_pplcy
+#4.Setting SSH parameter
+echo "`date +%D"-"%T`:Setting SSH parameter." >>$LOG
+./enb_ssh
+#5.Disable CTRL+ALT+DELETE
+echo "`date +%D"-"%T`:Disabling CTRL+ALT+DELETE." >>$LOG
+./disb_cad
+#6.Enabling Dynamic MOTD
+echo "`date +%D"-"%T`:Enabling Dynamic MOTD." >>$LOG
+./enb_motd
+#7.Disabling GUI
+echo "`date +%D"-"%T`:Disabling GUI." >>$LOG
+./disb_gui
+#8.Setting Banner
+echo "`date +%D"-"%T`:Enabling banner." >>$LOG
+./enb_bann
+#9.Setting BASH TIME format
+echo "`date +%D"-"%T`:Enabling BASH time format." >>$LOG
+./enb_batf
